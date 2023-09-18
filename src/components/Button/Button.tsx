a17c9156d06d0cd924ef6c96dd6afc25b28e455b
ashkanatutdev <ashkan.atut.dev@gmail.com>
2023-09-18 22:57:42 +0200

    v1.0.7 - Button fixes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import styled from "styled-components";
-import { ButtonProps } from "./Button.types";
+import { ButtonProps, Variants } from "./Button.types";
 
 const StyledButton = styled.button<ButtonProps>`
   border: 0;
   line-height: 1;
   font-size: 0.7rem;
-  cursor: pointer;
   font-weight: 700;
   font-weight: bold;
   border-radius: 3px;
   display: inline-block;
+  cursor: ${(props) =>
+  (!props.disabled
+    ? 'pointer' : 'default')};
   padding: ${(props) =>
     props.size === "small"
       ? "7px 25px 8px"
@@ -18,71 +20,69 @@ const StyledButton = styled.button<ButtonProps>`
         ? "9px 30px 11px"
         : "14px 30px 16px"};
     background-color: ${(props) =>
-    props.color === 'primary'
-      ? "#1a63b8"
-      : props.color === 'secondary'
-        ? "#656565"
-        : props.color === 'warning'
-          ? "#FAB848"
-          : props.color === 'error'
-            ? "#FA4848"
-            : props.color === 'info'
-              ? "#2d71a1"
-              : props.color === 'success'
-                ? "#52c86e"
-                : props.color === 'light'
-                  ? "#ececec"
-                  : "#2c2c2c"};
+  (props.color === 'primary'
+    ? Variants.primary
+    : props.color === 'secondary'
+      ? Variants.secondary
+      : props.color === 'warning'
+        ? Variants.warning
+        : props.color === 'error'
+          ? Variants.error
+          : props.color === 'info'
+            ? Variants.info
+            : props.color === 'success'
+              ? Variants.success
+              : props.color === 'light'
+                ? Variants.light
+                : Variants.dark)};
   opacity: ${(props) => (props.disabled ? 0.5 : 1)};
   color: ${(props) =>
-    props.color === 'light'
-      ? "#292929"
-      : "#f2f2f2"};
+  (props.color === 'light'
+    ? Variants.dark
+    : Variants.light)};
   &:hover {
     color: ${(props) =>
-    props.color === 'primary'
-      ? "#1a63b8"
-      : props.color === 'secondary'
-        ? "#656565"
-        : props.color === 'warning'
-          ? "#FAB848"
-          : props.color === 'error'
-            ? "#FA4848"
-            : props.color === 'info'
-              ? "#2d71a1"
-              : props.color === 'success'
-                ? "#52c86e"
-                : props.color === 'light'
-                  ? "#2e2e2e"
-                  : "#2c2c2c"};
+  (!props.disabled && props.color === 'primary'
+    ? Variants.primary
+    : !props.disabled && props.color === 'secondary'
+      ? Variants.secondary
+      : !props.disabled && props.color === 'warning'
+        ? Variants.warning
+        : !props.disabled && props.color === 'error'
+          ? Variants.error
+          : !props.disabled && props.color === 'info'
+            ? Variants.info
+            : !props.disabled && props.color === 'success'
+              ? Variants.success
+              : !props.disabled && props.color === 'light'
+                ? Variants.dark
+                : !props.disabled  && Variants.dark)};
     border: ${(props) =>
-    props.color === 'primary'
-      ? "#1a63b8 1px solid"
-      : props.color === 'secondary'
-        ? "#656565 1px solid"
-        : props.color === 'warning'
-          ? "#FAB848 1px solid"
-          : props.color === 'error'
-            ? "#FA4848 1px solid"
-            : props.color === 'info'
-              ? "#2d71a1 1px solid"
-              : props.color === 'success'
-                ? "#52c86e 1px solid"
-                : props.color === 'light'
-                  ? "#2e2e2e 1px solid"
-                  : "#2c2c2c 1px solid"};
-    background-color: white;
+  (!props.disabled && props.color === 'primary'
+    ? `${Variants.primary} 1px solid`
+    : !props.disabled && props.color === 'secondary'
+      ? `${Variants.secondary} 1px solid`
+      : !props.disabled && props.color === 'warning'
+        ? `${Variants.warning} 1px solid`
+        : !props.disabled && props.color === 'error'
+          ? `${Variants.error} 1px solid`
+          : !props.disabled && props.color === 'info'
+            ? `${Variants.info} 1px solid`
+            : !props.disabled && props.color === 'success'
+              ? `${Variants.success} 1px solid`
+              : !props.disabled && props.color === 'light'
+                ? `${Variants.dark} 1px solid`
+                : !props.disabled && `${Variants.dark} 1px solid`)};
+    background-color: ${(props) => (!props.disabled && Variants.light)};
     font-size: 0.7rem
   }
   &:active {
-    border: solid 2px #1b116e;
-    color: black;
     padding: ${(props) =>
-    props.size === "small"
+    !props.disabled && props.size === "small"
       ? "5px 23px 6px"
-      : props.size === "medium"
+      : !props.disabled && props.size === "medium"
         ? "7px 28px 9px"
-        : "12px 28px 14px"};
+        : !props.disabled && "12px 28px 14px"};
   }
 `;
 
--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -3,10 +3,20 @@ import { MouseEventHandler } from "react";
 type Colors = 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success' | 'light' | 'dark';
 
 export interface ButtonProps {
-  text?: string;
   color?: Colors
   disabled?: boolean;
   size?: "small" | "medium" | "large";
+  text?: string;
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
+export enum Variants {
+  primary = "#008ae6",
+  secondary = "#666699",
+  error = "#ff4d4d",
+  warning = "#ffa64d",
+  info = "#0099cc",
+  success = "#88cc00",
+  light = "#eff5f5",
+  dark = "#141f1f",
+}
